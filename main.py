fe69318b1f8b3eefd7706a1b3587c6fc353889ee
Heitor <102591958+HeitorWestphal@users.noreply.github.com>
2023-08-23 12:11:52 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+from keras.models import load_model
+import numpy as np
+import cv2
+import pyautogui
+import time
+
+model = load_model('Model/Keras_model.h5')
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+cap = cv2.VideoCapture(0)
+
+classes = ['Pausar','Up','Down','Right','Left','Nan']
+    
+def pause_video():
+    pyautogui.hotkey('space')
+    time.sleep(1)  
+    
+
+def down():
+    pyautogui.hotkey('down')
+
+
+def up():
+    pyautogui.hotkey('up')
+
+
+def left():
+    pyautogui.hotkey('left')
+    time.sleep(0.5)
+
+def right():
+    pyautogui.hotkey('Right')
+    time.sleep(0.5)
+
+
+
+while True:
+    success,img = cap.read()
+    imgS = cv2.resize(img, (224, 224))
+    image_array = np.asarray(imgS)
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+    prediction = model.predict(data)
+    indexVal = np.argmax(prediction)
+
+    cv2.putText(img, str(classes[indexVal]),(50,50), cv2.FONT_HERSHEY_COMPLEX, 2, (0,255,0), 2)
+    print(classes[indexVal])
+
+    if classes[indexVal] == 'Pausar' :
+       pause_video()
+
+    elif classes[indexVal] == 'Down' :
+        down()   
+
+    elif classes[indexVal] == 'Up' :
+        up()
+
+    elif classes[indexVal] == 'Left' :
+        left()
+
+    elif classes[indexVal] == 'Right' :
+        right()        
+       
+
+    cv2.imshow('img',img)
+    cv2.waitKey(1)
+
